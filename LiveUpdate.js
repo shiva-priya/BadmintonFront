dd138ad29436318053cb875fc31c983764b0631f
priyad <shiva.priya@pramati.com>
2020-01-16 17:57:25 +0530

    New Files for other functions of Admin

--- LiveUpdate.js
+++ LiveUpdate.js
@@ -1,23 +1,29 @@
+//root
+var maindiv = document.createElement('div');
+maindiv.setAttribute('class','main');
+document.body.appendChild(maindiv);
+
+
+//staring three parts div
 var liveupdateContainer = document.createElement('div');
-document.body.appendChild(liveupdateContainer);
+liveupdateContainer.setAttribute('class','starting');
+maindiv.appendChild(liveupdateContainer);
 
 
 
 //matchnumber
 
 var matchno=document.createElement('p');
-matchno.textContent="Match no:";
+matchno.textContent="Match ID:";
 
 
 var matchnobox = document.createElement('input');
 matchnobox.setAttribute('type','text');
-var  ButtonM = document.createElement('input');
-ButtonM.setAttribute('type','submit');
-ButtonM.setAttribute('value','submit');
+matchnobox.setAttribute('class','textbox');
 
 liveupdateContainer.appendChild(matchno);
 matchno.appendChild(matchnobox);
-matchnobox.appendChild(ButtonM);
+
 
 //select teams
 
@@ -26,7 +32,7 @@ match.textContent="Match between";
 var dropdown=document.createElement('select');
 dropdown.setAttribute('class','dropbox');
 var option=document.createElement('option');
-option.textContent="select Team A";
+option.textContent="Team A";
 var option1=document.createElement('option');
 option1.textContent="Team 1";
 var option2=document.createElement('option');
@@ -50,7 +56,7 @@ var dropdown2=document.createElement('select');
 dropdown2.setAttribute('class','dropbox');
 
 var option_t=document.createElement('option');
-option_t.textContent="select Team B";
+option_t.textContent="Team B";
 var option_1=document.createElement('option');
 option_1.textContent="Team 1";
 var option_2=document.createElement('option');
@@ -74,25 +80,29 @@ dropdown2.appendChild(option_4);
 //select players
 
 var players=document.createElement('p');
-players.textContent="select players";
+players.textContent="Select Players";
 
 
 var dropdownp1=document.createElement('select');
 dropdownp1.setAttribute('class','dropbox');
+
 var option_pta=document.createElement('option');
-option_pta.textContent="select playerfrom Team A";
+option_pta.textContent="Team A";
+
 var option1_pta=document.createElement('option');
 option1_pta.textContent="player 1";
+
 var option2_pta=document.createElement('option');
 option2_pta.textContent="player 2";
 
 var option3_pta=document.createElement('option');
 option3_pta.textContent="player 3";
+
 var option4_pta=document.createElement('option');
 option4_pta.textContent="player 4";
+
 liveupdateContainer.appendChild(players);
 liveupdateContainer.appendChild(dropdownp1);
-
 dropdownp1.appendChild(option_pta);
 dropdownp1.appendChild(option1_pta);
 dropdownp1.appendChild(option2_pta);
@@ -103,7 +113,7 @@ dropdownp1.appendChild(option4_pta);
 var dropdownp2=document.createElement('select');
 dropdownp2.setAttribute('class','dropbox');
 var option_ptb=document.createElement('option');
-option_ptb.textContent="select playerfrom Team B";
+option_ptb.textContent="Team B";
 var option1_ptb=document.createElement('option');
 option1_ptb.textContent="player 1";
 var option2_ptb=document.createElement('option');
@@ -124,26 +134,31 @@ dropdownp2.appendChild(option4_ptb);
 
 
 
-
+//sets
 
 var pointstable=document.createElement("div")
 pointstable.setAttribute('class','pointstable');
 pointstable.style.padding = "20px";
 var setLabel = document.createElement('label');
-setLabel.textContent = "set1 : ";
+setLabel.textContent = "SET 1 : ";
 var pointstext1 = document.createElement('input');
 pointstext1.setAttribute('type','text');
+pointstext1.setAttribute('class','textbox');
+
 var pointstext2 = document.createElement('input');
 pointstext2.setAttribute('type','text');
+pointstext2.setAttribute('class','textbox');
 pointstext2.style.margin = "20px";
 var pointstext3 = document.createElement('input');
 pointstext3.setAttribute('type','text');
+pointstext3.setAttribute('placeholder','RESULT');
 var  Button3 = document.createElement('input');
+pointstext3.setAttribute('class','textbox');
 Button3.setAttribute('type','submit');
 Button3.setAttribute('value','submit');
+Button3.setAttribute('class','button');
 
-
-document.body.appendChild(pointstable);
+maindiv.appendChild(pointstable);
 pointstable.appendChild(setLabel);
 pointstable.appendChild(pointstext1);
 pointstable.appendChild(pointstext2);
@@ -154,19 +169,24 @@ var pointstable2=document.createElement("div")
 pointstable2.setAttribute('class','pointstable2');
 pointstable2.style.padding = "20px";
 var setLabel2 = document.createElement('label');
-setLabel2.textContent = "set2 : ";
+setLabel2.textContent = "SET 2 : ";
 var pointstext_1 = document.createElement('input');
 pointstext_1.setAttribute('type','text');
+pointstext_1.setAttribute('class','textbox');
 var pointstext_2 = document.createElement('input');
 pointstext_2.setAttribute('type','text');
 pointstext_2.style.margin = "20px";
+pointstext_2.setAttribute('class','textbox');
 var pointstext_3 = document.createElement('input');
 pointstext_3.setAttribute('type','text');
+pointstext_3.setAttribute('class','textbox');
+pointstext_3.setAttribute('placeholder','RESULT');
 var  Button2 = document.createElement('input');
+Button2.setAttribute('class','button');
 Button2.setAttribute('type','submit');
 Button2.setAttribute('value','submit');
 
-document.body.appendChild(pointstable2);
+maindiv.appendChild(pointstable2);
 pointstable2.appendChild(setLabel2);
 pointstable2.appendChild(pointstext_1);
 pointstable2.appendChild(pointstext_2);
@@ -178,21 +198,26 @@ var pointstable3=document.createElement("div");
 pointstable3.setAttribute('class','pointstable3');
 pointstable3.style.padding = "20px";
 var setLabel3 = document.createElement('label');
-setLabel3.textContent = "set3 : ";
+setLabel3.textContent = "SET 3 : ";
 var pointstext1_1 = document.createElement('input');
 pointstext1_1.setAttribute('type','text');
+pointstext1_1.setAttribute('class','textbox');
 var pointstext1_2 = document.createElement('input');
 pointstext1_2.setAttribute('type','text');
+pointstext1_2.setAttribute('class','textbox');
 pointstext1_2.style.margin = "20px";
 var pointstext1_3 = document.createElement('input');
 pointstext1_3.setAttribute('type','text');
-var  signUpButton = document.createElement('input');
-signUpButton.setAttribute('type','submit');
-signUpButton.setAttribute('value','submit');
-document.body.appendChild(pointstable3);
+pointstext1_3.setAttribute('class','textbox');
+pointstext1_3.setAttribute('placeholder','RESULT');
+var  sButton = document.createElement('input');
+sButton.setAttribute('class','button');
+sButton.setAttribute('type','submit');
+sButton.setAttribute('value','submit');
+maindiv.appendChild(pointstable3);
 pointstable3.appendChild(setLabel3);
 pointstable3.appendChild(pointstext1_1);
 pointstable3.appendChild(pointstext1_2);
 pointstable3.appendChild(pointstext1_3);
 
-pointstable3.appendChild(signUpButton);
+pointstable3.appendChild(sButton);
